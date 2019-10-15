66f9235db08f4598c2372d97812de79520898c39
KatyTao <katyt3253@gmail.com>
2019-10-15 15:27:20 +0800

    exercise16

--- week-01/day-02/exercise12-17.js
+++ week-01/day-02/exercise12-17.js
@@ -19,9 +19,49 @@ console.log(doubling(ak));
 let a1 = `EPAM`;
 function greet(parm1) {
     if (a1 !== ``) {
-        return `Greetings, dear ` + parm1.toString();
+        return `Greetings, dear ${parm1}`;
     }else {
         return `Please input your name`;
     }
 }
 console.log(greet(a1));
+
+//14 - append-s
+'use strict';
+
+// - Create a variable named `am` and assign the value `dog` to it
+// - Write a function called `appendS` that gets a string as an input
+//   and appends an 's' character to its end
+// - Print the result of `appendS(am)` to the console
+let am = `dog`;
+function appendS(parm1) {
+    return parm1.toString() + `S`;
+}
+console.log(appendS(am));
+
+//15 - sum
+'use strict';
+// - Write a function called `sum` that sum all the numbers until the given parameter
+// - The function should return the result
+function sum() {
+    let sum = 0;
+    len = arguments.length;
+    for (let i = 0; i < len; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+console.log(sum(2,3,4));
+
+//16 - factorio
+'use strict';
+// - Create a function called `factorio`
+//   that returns it's input's factorial
+function factorio(parm1) {
+    if (parm1 === 1) {
+        return 1;
+    }else {
+        return parm1 * factorio(parm1-1);
+    }
+}
+console.log(factorio(3));
