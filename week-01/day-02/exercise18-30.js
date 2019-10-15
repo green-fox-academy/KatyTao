22f88dff0c213a78e9a8d15be419af25591419de
KatyTao <katyt3253@gmail.com>
2019-10-15 16:19:54 +0800

    exercise23

--- week-01/day-02/exercise18-30.js
+++ week-01/day-02/exercise18-30.js
@@ -15,6 +15,53 @@ console.log(magicNumbers[2]);
 // - Log to the console if `p2` has more elements than `p1`
 let p1 = [1, 2, 3];
 let p2 = [4, 5];
-if (p2.length() > p1.length()) {
+if (p2.length > p1.length) {
     console.log(p2);
 }
+
+//20 - sum-elements
+'use strict';
+// - Create a variable named `r` with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+let r = [54, 23, 66, 12];
+console.log(r[1]+r[2]);
+
+//21 - swap-elements
+'use strict';
+
+// - Create a variable named `abc` with the following content: `['Arthur', 'Boe', 'Chloe']`
+// - Swap the first and the third element of `abc`
+let abc = ['Arthur', 'Boe', 'Chloe'];
+let temp = abc[0];
+abc[0] = abc[2];
+abc[2] = temp;
+console.log(abc);
+
+
+//22 - print-all
+  
+'use strict';
+
+// - Create a variable named `af` with the following content: `[4, 5, 6, 7]`
+// - Log each the element of `af` to the console*
+// *hint: use a loop, console.log(af) won't cut it
+// - bonus for using the correct built in array method
+let af = [4, 5, 6, 7];
+af.forEach(item  => console.log(item));
+
+
+//23 - change-element
+
+'use strict';
+
+// - Create an array named `s` with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4 with the `.map` method 
+// - Print the fourth element as a test
+let s = [1, 2, 3, 8, 5, 6];
+s = s.map(x => {
+    if (x === 8){
+        return 4;
+    }
+    return x;
+})
+console.log(s[3]);
