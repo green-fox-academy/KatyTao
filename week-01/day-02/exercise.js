17d8a99ed42f446a2275fbd514a6361bebce38ff
KatyTao <katyt3253@gmail.com>
2019-10-15 13:59:43 +0800

    exercise3

--- week-01/day-02/exercise.js
+++ week-01/day-02/exercise.js
@@ -68,4 +68,11 @@ while (count < 100) {
     count ++;
 }
 
-//
+//3 - print-even
+'use strict';
+// Create a program that prints all the even numbers between 0 and 500
+for (let a = 0; a <= 500;  a++) {
+    if (a % 2 === 0) {
+        console.log(a);
+    }
+}
