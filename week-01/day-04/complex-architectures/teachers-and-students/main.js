e1f6a7e42774e27a8cd60a134097677271c1b512
KatyTao <katyt3253@gmail.com>
2019-10-17 16:02:54 +0800

    finish teacher and student

---
+++ week-01/day-04/complex-architectures/teachers-and-students/main.js
@@ -0,0 +1,24 @@
+class student{
+    learn() {
+        console.log(`student is learning something new`);
+    }
+    question(teacher) {
+        this.teacher = teacher;
+        this.teacher.answer();
+    }
+}
+
+class teacher {
+    teach(student) {
+        this.student = student;
+        this.student.learn();
+    }
+    answer() {
+        console.log(`the teacher is answering a question`);   
+    }
+}
+
+const stu = new student();
+const tea = new teacher();
+stu.question(tea);
+tea.teach(stu);
---
+++ week-01/day-04/complex-architectures/teachers-and-students/students.js
@@ -0,0 +1,11 @@
+class student{
+    constructor(name) {
+        this.name = name;
+    }
+    learn() {
+        console.log(`${this.name} is learning something new`);
+    }
+    question(teacher) {
+        
+    }
+}
