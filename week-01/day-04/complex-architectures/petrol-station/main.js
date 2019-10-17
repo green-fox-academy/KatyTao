f2a636409b30276da1a1c8d2e16d37de1fb681e1
KatyTao <katyt3253@gmail.com>
2019-10-17 16:22:40 +0800

    complete petrol station

---
+++ week-01/day-04/complex-architectures/petrol-station/main.js
@@ -0,0 +1,24 @@
+class Station {
+    constructor(gasAmount = 500) {
+        this.gasAmount = gasAmount;
+    }
+    refill(car) {
+        this.gasAmount -= car.capacity;
+        car.gasAmount += car.capacity;
+    }
+}
+
+class Car {
+    constructor (gasAmount = 0, capacity = 100){
+        this.gasAmount = gasAmount;
+        this.capacity = capacity;
+    }
+}
+
+const stationOne = new Station();
+const carOne = new Car();
+console.log(stationOne);
+console.log(carOne);
+stationOne.refill(carOne);
+console.log(stationOne);
+console.log(carOne);
