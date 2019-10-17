a86a654756ae9e6ee37f15d7348f143cf0e5682f
KatyTao <katyt3253@gmail.com>
2019-10-17 17:43:57 +0800

    finish farm

---
+++ week-01/day-04/getters-and-setters/animal.js
@@ -0,0 +1,20 @@
+class Animal {
+    constructor (hunger = 50,thirst = 50) {
+        this.hunger = hunger;
+        this.thirst = thirst;
+    }
+    eat() {
+        return this.hunger--;
+    }
+    drink() {
+        return this.thirst--;
+    }
+    play() {
+        return this.hunger++,this.thirst++;
+    }
+}
+
+const dog = new Animal();
+dog.play();
+console.log(dog);
+module.exports = Animal;
