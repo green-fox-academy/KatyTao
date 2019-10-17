a72e32f1133704fd52192a2632879d8b19af12b3
KatyTao <katyt3253@gmail.com>
2019-10-17 13:26:28 +0800

    finish counter

---
+++ week-01/day-04/encapsulation&constructor/counter.js
@@ -0,0 +1,18 @@
+class Counter {
+    constructor(number = 0) {
+        this.number = number;
+        this.initNum = number;
+    }
+    add(number = 1) {
+        return this.number += number;
+    }
+    get() {
+        return toString(this.number);
+    }
+    reset() {
+        return this.initNum;
+    }
+}
+
+const counterOne = new Counter(3);
+
