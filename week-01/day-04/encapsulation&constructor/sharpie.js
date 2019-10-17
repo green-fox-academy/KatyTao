92fef33e074c25df3318c1cd1f3df6996526d4d5
KatyTao <katyt3253@gmail.com>
2019-10-17 11:45:45 +0800

    finish sharpie

---
+++ week-01/day-04/encapsulation&constructor/sharpie.js
@@ -0,0 +1,12 @@
+'use strict'
+class sharpie {
+    constructor(color, width, inkAmount = 100) {
+        this.color = color;
+        this.width = width;
+        this.inkAmount = inkAmount;
+    }
+    use() {
+        return this.inkAmount--;
+    }
+}
+
