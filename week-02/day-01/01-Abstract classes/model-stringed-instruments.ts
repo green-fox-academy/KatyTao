e4d9361414b9d6bbba74f631f3c26fe17fe817fc
KatyTao <katyt3253@gmail.com>
2019-10-21 13:06:54 +0800

    implement requests by instruments.ts and main.ts

---
+++ week-02/day-01/01-Abstract classes/model-stringed-instruments.ts
@@ -0,0 +1,54 @@
+`use strict`
+abstract class Instrument {
+  public name:string;
+  abstract play():void;
+}
+
+abstract class StringedInstrument extends Instrument {
+  protected numberOfStrings:number;
+  abstract sound():string;
+}
+
+class ElectricGuitar extends StringedInstrument {
+  soundName: string;
+  constructor(numberOfStrings: number = 6) {
+    super();
+    this.numberOfStrings = numberOfStrings;
+  }
+  sound() {
+    return `Twang`;
+  }
+  play(): void {
+    
+    console.log(`Electric Guitar, a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`);
+  }
+}
+
+class BassGuitar extends StringedInstrument {
+  soundName: string;
+  constructor(numberOfStrings: number = 4) {
+    super();
+    this.numberOfStrings = numberOfStrings;
+  }
+  sound(): string {
+    return `Duum-duum-duum`;
+  }
+  play(): void {
+    console.log(`Bass Guitar, a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`);
+  }
+}
+
+class Violin extends StringedInstrument {
+  soundName:string;
+  constructor(numberOfStrings: number = 4) {
+    super();
+    this.numberOfStrings = numberOfStrings;
+  }
+  sound(): string {
+    return `Screech`;
+  }
+  play(): void {
+    console.log(`Violin, a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`);
+  }
+}
+export{ElectricGuitar, BassGuitar, Violin};
