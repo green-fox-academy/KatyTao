fdabfd2a05238d4e890b07fbd48e4b702be021fc
KatyTao <katyt3253@gmail.com>
2019-10-30 13:22:04 +0800

    finish exercise 3 with element attributes

---
+++ week-03/day-03/03-elementAttributes/attribute-1.js
@@ -0,0 +1,19 @@
+// Write the image's url to the console.
+
+console.log(document.querySelector('img').src);
+
+// Replace the image with a picture of your favorite animal.
+
+document.querySelector("img").src = "https://alaynakbaker.files.wordpress.com/2016/12/golden.jpg";
+
+// Make the link point to the Green Fox Academy website.
+
+document.querySelector("a").href = "https://www.greenfoxacademy.com/";
+
+// Disable the second button.
+
+document.querySelectorAll("button")[1].disabled = true;
+
+// Replace its text with 'Don't click me!'.
+
+document.querySelectorAll("button")[1].textContent = "Don't click me!";
