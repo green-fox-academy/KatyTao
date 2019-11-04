3adc86adf6e5941aa08c0c8c78eec9a2c5f4cd28
KatyTao <katyt3253@gmail.com>
2019-11-04 17:52:10 +0800

    update todo with structure

--- week-04/day-01/todo-app/src/App.js
+++ week-04/day-01/todo-app/src/App.js
@@ -2,25 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class TodoApp extends React.Component{
+  constructor() {
+    this.state = {
+      todo: [
+        {
+          text: 'Feed the monkey',
+          prio: 2
+        },
+        {
+          text: 'Buy milk',
+          prio: 1
+        }
+      ],
+      doneTodo: [
+        {
+          text: 'Sleep 8 hours',
+          prio: 2
+        },
+        {
+          text: 'Mentoring others',
+          prio: 1
+        }
+      ],
+    }
+  }
+
+  doneTodo() {
+
+  }
+  
+  deleteTodo() {
+
+  }
+
+  render() {
+    return;
+  }
 }
 
-export default App;
