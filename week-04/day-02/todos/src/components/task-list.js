4697a8d1662eaeec7fa85eb5acdf39fb7a20983a
Katy <katyt3253@gmail.com>
2019-11-05 22:43:52 +0800

    update click to change image

--- week-04/day-02/todos/src/components/task-list.js
+++ week-04/day-02/todos/src/components/task-list.js
@@ -1,32 +1,44 @@
-import React from 'react';
-import Button from './button';
-import img from '../img/bin.svg';
-export default class TaskList extends React.Component{
+import React from "react";
+import BinImg from "../img/bin.svg";
+import CheckImg from "../img/check.svg";
+import BinHoverImg from "../img/bin-hover.svg";
+import CheckHoverImg from "../img/check-hover.svg";
+
+export default class TaskList extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
     this.handleBinClick = this.handleBinClick.bind(this);
-  }  
+    this.handleBinClick = this.handleBinClick.bind(this);
+  }
 
   handleBinClick(event) {
-    console.log(event.target);
-    event.target.style.backgroundImage = "url('./img/bin-hover.svg')";
+    event.target.src = BinHoverImg;
   }
-  
-  render(){
-    return (
-      this.props.items.map((item,index) =>
-        <div className="todo-item" key={index}>
-          <p>{item.text}</p>
-          <div className="icon-list">
-            <Button class="bin-icon" onClick={this.handleBinClick}/>
-            <Button class="check-icon" onClick=""/>
-            <img class="bin-icon" src={img} alt=""/>
-            <img class="check-icon" src={} alt=""/>
-          </div>
+
+  handleCheckClick(event) {
+    event.target.src = CheckHoverImg;
+  }
+
+  render() {
+    return this.props.items.map((item, index) => (
+      <div className="todo-item" key={index}>
+        <p>{item.text}</p>
+        <div className="icon-list">
+          <img
+            className="bin-icon"
+            onClick={this.handleBinClick}
+            src={BinImg}
+            alt="bin icon"
+          />
+          <img
+            className="check-icon"
+            onClick={this.handleCheckClick}
+            src={CheckImg}
+            alt="check icon"
+          />
         </div>
-      )
-    )
+      </div>
+    ));
   }
-  
 }
