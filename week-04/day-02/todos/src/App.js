cbbfb224ccf214c12ce3c4b073c939b711135c08
KatyTao <katyt3253@gmail.com>
2019-11-05 16:38:45 +0800

    finish styling for todo list

--- week-04/day-02/todos/src/App.js
+++ week-04/day-02/todos/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from './components/button'
+import TaskList from './components/taskList';
 import './App.css';
+export default class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      list:[
+        {
+          text: 'Feed the monkey',
+          todo: 1,
+        },
+        {
+          text: 'Buy milk',
+          todo:0,
+        },
+        {
+          text: 'Sleep 8 hours',
+          todo: 1,
+        },
+        {
+          text: 'Mentoring others',
+          todo: 1,
+        }
+    ]
+  }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleSubmit(event) {
+    console.log();
+  }
+
+  render() {
+    return(
+      <main>
+        <h1 className="heading">TODOS</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <Button onClick={this.handleSubmit} text="Add"/>
+        </form>
+        <section>
+          <TaskList items={this.state.list}/> 
+        </section>
+      </main>
+    )
+  }
 }
-
-export default App;
