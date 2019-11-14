abfb8f093aee14aac850e0f21cff04e751164e4d
KatyTao <katyt3253@gmail.com>
2019-11-14 14:10:38 +0800

    complete todos with ejs

--- week-05/day-04/todos/index.js
+++ week-05/day-04/todos/index.js
@@ -5,16 +5,25 @@ const PORT = 3000;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+const todos = [
+  'get up',
+  'survive',
+  'go back to bed',
+];
 
-// home page
 app.get('/', (req,res) => {
-  //render 'home.ejs'
-  const { name } = req.query;
-  const user = name!==undefined? name: 'Guest';
-  res.render('home',{
-    title: `Welcome back, ${user}!`,
-  })
+  res.render('home', {todos:todos});
 })
+
+// // home page
+// app.get('/', (req,res) => {
+//   //render 'home.ejs'
+//   const { name } = req.query;
+//   const user = name!==undefined? name: 'Guest';
+//   res.render('home',{
+//     title: `Welcome back, ${user}!`,
+//   })
+// })
 
 // start express app on port 3000
 app.listen(PORT, () => {
