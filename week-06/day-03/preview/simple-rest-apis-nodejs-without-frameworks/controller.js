29f5c89be9f1bf98bf31ccde34f74c6d09acde8f
KatyTao <katyt3253@gmail.com>
2019-11-20 10:16:57 +0800

    format controllers

--- week-06/day-03/preview/simple-rest-apis-nodejs-without-frameworks/controller.js
+++ week-06/day-03/preview/simple-rest-apis-nodejs-without-frameworks/controller.js
@@ -3,12 +3,11 @@ const url = require("url"); //helps in parsing urls
 
 module.exports = http.createServer((req, res) => {
   var service = require("./service.js");
-  const reqUrl = url.parse(req.url, true);
+  const reqUrl = url.parse(req.url, true); //true is used to parse the query string
 
   // GET Endpoint
   if (reqUrl.pathname == "/sample" && req.method === "GET") {
     console.log("Request Type:" + req.method + " Endpoint: " + reqUrl.pathname);
-
     service.sampleRequest(req, res);
     // POST Endpoint
   } else if (reqUrl.pathname == "/test" && req.method === "POST") {
